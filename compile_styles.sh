0f7dec1d0689aa6a1274beb7cc964e9db16e2a19
cconard96 <cconard96@gmail.com>
2021-09-17 19:59:06 -0400

    Use sass instead of node-sass

--- compile_styles.sh
+++ compile_styles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Compiling themes";
-node-sass src/themes/blood_moon.scss -o dist/;
-node-sass src/themes/dimmed.scss -o dist/;
-node-sass src/themes/midnight.scss -o dist/;
+sass src/themes/blood_moon.scss dist/blood_moon.css;
+sass src/themes/dimmed.scss dist/dimmed.css;
+sass src/themes/midnight.scss dist/midnight.css;
 echo "Done!";
